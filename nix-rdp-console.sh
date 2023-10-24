c9c63ae8b24c7b446e21e8718bebf90a43cc1a5e
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2023-10-24 13:45:14 -0600

    Script fix.

--- nix-rdp-console.sh
+++ nix-rdp-console.sh
@@ -5,8 +5,8 @@ sudo apt install -y wireshark netcat
 
 # Download the Wireshark configuration script to the desktop
 sudo mkdir /home/pslearner/Desktop
-sudo curl -o /home/pslearner/Desktop/wireshark-configure.sh 
-sudo chmod +x "/home/pslearner/Desktop/wireshark-configure.sh" "https://raw.githubusercontent.com/ps-interactive/lab_security-lab-audition-example/main/wireshark-configure.sh"
+sudo curl -o "/home/pslearner/Desktop/wireshark-configure.sh" "https://raw.githubusercontent.com/ps-interactive/lab_security-lab-audition-example/main/wireshark-configure.sh"
+sudo chmod +x /home/pslearner/Desktop/wireshark-configure.sh
 
 # Run Commands/Scripts
 # Need to set up a listener on the port for demo purposes.
